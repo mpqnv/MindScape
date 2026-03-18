1602fa5d9680efb9b77d58920394005f5936267e
johnyildirim <johnyildr53@gmail.com>
2026-03-18 00:09:03 -0400

    quiz.html and quiz.css updated + quiz.js created

---
+++ JavaScript/quizResult.js
@@ -0,0 +1,26 @@
+function quizResult(){
+    let questions = ["q1", "q2", "q3", "q4", "q5"];
+    let totalScore = 0;
+    let result = "";
+    for (let i = 0; i < questions.length; i++){
+        let options = document.getElementsByName(questions[i]);
+        let chosenOption = 0;
+        for (let j = 0; j < options.length; j++){
+            if (options[j].checked){
+                chosenOption = options[j].value;
+            }
+        }
+    totalScore += Number(chosenOption);
+    }
+    
+    if (totalScore <= 5){
+        result = "Your mental health is in a good state! Keep up the good work!";
+    } 
+    else if (totalScore <= 10){
+        result = "Your mental health is moderately well, but there is room for improvement. Consider: <tips>";
+    } 
+    else {
+        result = "Your mental health needs attention. Consider: <tips>";
+    }
+    document.getElementById("result-div").innerHTML = result;
+}
